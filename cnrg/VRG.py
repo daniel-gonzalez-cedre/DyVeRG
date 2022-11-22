[bug fix] Fixed an oversight in the edit distance computation when merging rules.
 Changes to be committed:
	modified:   cnrg/VRG.py
	modified:   notebooks/experiment_sequential.ipynb
	modified:   src/grammar_transitions.py
	modified:   src/rule_transitions.py

cnrg/VRG.py
@@ -5,12 +5,13 @@ refactored VRG
 # import copy
 # from typing import List, Dict
 
+from joblib import Parallel, delayed
 import numpy as np
 import networkx.algorithms.isomorphism as iso
 from tqdm import tqdm
 
 from cnrg.Rule import PartRule
-from src.utils import replace
+from src.utils import replace, graph_edit_distance
 
 
 class VRG:
@@ -65,47 +66,27 @@ class VRG:
             self.dl += rule.dl
         return self.dl
 
-    def conditional_matrix_ll(self, axis: str = 'col'):
-        assert axis in ['row', 'col']
-        rule_matrix = self.temporal_matrix.copy()
-        ll = 0
+    def minimum_edit_dist(self, rule: PartRule, parallel: bool = True, n_jobs: int = 4) -> int:
+        if rule.lhs in self.rule_dict:
+            candidates = [r for r in self.rule_dict[rule.lhs] if r is not rule]
+            penalty = 0
+        else:
+            candidates = [r for r in self.rule_list if r is not rule]
+            penalty = 1  # penalty for having to also modify the LHS symbol
 
-        for idx, _ in enumerate(self.rule_list):
-            if axis == 'col':
-                ax = rule_matrix[idx, :].copy()
-            else:
-                ax = rule_matrix[:, idx].copy()
+        if parallel:
+            edit_dists = Parallel(n_jobs=n_jobs)(
+                delayed(graph_edit_distance)(r.graph, rule.graph) for r in candidates
+            )
+        else:
+            edit_dists = [graph_edit_distance(r.graph, rule.graph) for r in candidates]
 
-            if len(ax[ax > 0]) > 0:
-                ax = ax / ax.sum()
-                ll += np.log(ax[ax > 0]).sum()
-            else:
-                pass
+        return int(min(edit_dists)) + penalty
 
-        return ll
-
-    # ignore this for now
-    # def compute_transition_matrix(self):
-    #     n = len(self.rule_list)
-    #     self.transition_matrix = np.zeros((n, n), dtype=float)
-
-    #     for child_idx, child_rule in tqdm(enumerate(self.rule_list), total=n):
-    #         for rule, source_idx, _ in self.rule_tree:
-    #             if source_idx is not None and child_rule == rule:
-    #                 parent_rule, _, _ = self.rule_tree[source_idx]
-    #                 parent_idx = self.rule_list.index(parent_rule)
-    #                 self.transition_matrix[parent_idx][child_idx] += 1
-
-    #     return
-
-    def init_temporal_matrix(self):
-        n = len(self.rule_list)
-        self.temporal_matrix = np.identity(n, dtype=float)
-
-        for idx, rule in enumerate(self.rule_list):
-            self.temporal_matrix[idx, idx] *= rule.frequency
-
-        return
+    def replace_rule(self, old_rule: PartRule, new_rule: PartRule):
+        replace(old_rule, new_rule, self.rule_list)
+        replace(old_rule, new_rule, self.rule_dict)
+        replace(old_rule, new_rule, self.rule_tree)
 
     def copy(self):
         bag = [rule.copy() for rule in self.rule_list]
@@ -126,11 +107,6 @@ class VRG:
         vrg_copy.temporal_matrix = self.temporal_matrix.copy() if self.temporal_matrix is not None else None
 
         return vrg_copy
-
-    def replace_rule(self, old_rule, new_rule):
-        replace(old_rule, new_rule, self.rule_list)
-        replace(old_rule, new_rule, self.rule_dict)
-        replace(old_rule, new_rule, self.rule_tree)
 
     def __len__(self):
         return len(self.rule_list)
@@ -205,3 +181,45 @@ class VRG:
         self.rule_list.append(rule)
         self.rule_dict[rule.lhs].append(rule)
         return rule.id
+
+    def init_temporal_matrix(self):
+        n = len(self.rule_list)
+        self.temporal_matrix = np.identity(n, dtype=float)
+
+        for idx, rule in enumerate(self.rule_list):
+            self.temporal_matrix[idx, idx] *= rule.frequency
+
+        return
+
+    # def conditional_matrix_ll(self, axis: str = 'col'):
+    #     assert axis in ['row', 'col']
+    #     rule_matrix = self.temporal_matrix.copy()
+    #     ll = 0
+
+    #     for idx, _ in enumerate(self.rule_list):
+    #         if axis == 'col':
+    #             ax = rule_matrix[idx, :].copy()
+    #         else:
+    #             ax = rule_matrix[:, idx].copy()
+
+    #         if len(ax[ax > 0]) > 0:
+    #             ax = ax / ax.sum()
+    #             ll += np.log(ax[ax > 0]).sum()
+    #         else:
+    #             pass
+
+    #     return ll
+
+    # ignore this for now
+    # def compute_transition_matrix(self):
+    #     n = len(self.rule_list)
+    #     self.transition_matrix = np.zeros((n, n), dtype=float)
+
+    #     for child_idx, child_rule in tqdm(enumerate(self.rule_list), total=n):
+    #         for rule, source_idx, _ in self.rule_tree:
+    #             if source_idx is not None and child_rule == rule:
+    #                 parent_rule, _, _ = self.rule_tree[source_idx]
+    #                 parent_idx = self.rule_list.index(parent_rule)
+    #                 self.transition_matrix[parent_idx][child_idx] += 1
+
+    #     return

src/rule_transitions.py
@@ -1,5 +1,4 @@
 import sys
-import copy
 import numpy as np
 
 sys.path.append('../')
@@ -35,15 +34,6 @@ def mutate_rule_domestic(grammar: VRG, u: int, v: int, mode: str, time: int) -> 
     ancestor_u = list(parent_rule.graph.nodes())[which_u]
     ancestor_v = list(parent_rule.graph.nodes())[which_v]
 
-    # decouple = (parent_rule.time_changed < time) and False  # TODO: debugging
-
-    # if the rule to be modified was learned at a prior timestep, preserve it
-    # otherwise, directly modify that rule and overwrite it
-    # if decouple:
-    #     new_rule = copy.deepcopy(parent_rule)
-    # else:
-    #     new_rule = parent_rule
-
     new_rule = parent_rule.copy()
     new_rule.time_changed = time
     if mode == 'add':
@@ -56,7 +46,7 @@ def mutate_rule_domestic(grammar: VRG, u: int, v: int, mode: str, time: int) -> 
     else:
         raise AssertionError(f'<<mode>> must be either "add" or "del"; found mode={mode} instead')
 
-    incorporate_rule(grammar, parent_rule, new_rule, which_parent)
+    incorporate_rule(grammar, parent_rule, new_rule, which_parent, 1)
     return grammar
 
 
@@ -80,15 +70,6 @@ def mutate_rule_diplomatic(grammar: VRG, u: int, v: int, time: int) -> VRG:
     parent_rule, which_parent, which_u = ancestor(u, grammar)
     ancestor_u = list(parent_rule.graph.nodes())[which_u]
 
-    # decouple = (parent_rule.time_changed < time) and False  # TODO: debugging
-
-    # if the rule to be modified was learned at a prior timestep, preserve it
-    # otherwise, directly modify that rule and overwrite it
-    # if decouple:
-    #     new_rule = copy.deepcopy(parent_rule)
-    # else:
-    #     new_rule = parent_rule
-
     new_rule = parent_rule.copy()
     new_rule.time_changed = time
     new_rule.graph.add_node(v, b_deg=0)
@@ -97,71 +78,72 @@ def mutate_rule_diplomatic(grammar: VRG, u: int, v: int, time: int) -> VRG:
     grammar.rule_source[v] = grammar.rule_source[u]  # rule_source now points to the same location in the rule_tree for u and v
     grammar.which_rule_source[v] = list(new_rule.graph.nodes()).index(v)  # compute the pointer for v in the NEW rule's RHS
 
-    incorporate_rule(grammar, parent_rule, new_rule, which_parent)
+    incorporate_rule(grammar, parent_rule, new_rule, which_parent, 2)
     return grammar
 
 
-def incorporate_rule(grammar: VRG, parent_rule: PartRule, new_rule: PartRule, which_parent: int,
-                     decouple: bool = False, mode: str = 'iso'):
-    assert mode in ['iso', 'hash']
-
-    def is_isomorphic(r1, r2):
-        return r1 == r2
+def incorporate_rule(grammar: VRG, parent_rule: PartRule, new_rule: PartRule, which_parent: int, penalty: int):
+    assert penalty in [1, 2]
 
     # check to see if this rule already exists in the grammar
-    if mode == 'iso':
-        parent_idx = find(parent_rule, grammar.rule_list)[0]
+    parent_idx = find(parent_rule, grammar.rule_list)[0]
 
-        if new_rule.lhs in grammar.rule_dict:
-            for rule in grammar.rule_dict[new_rule.lhs]:
-                # if new_rule == rule:
-                message, result = timeout(is_isomorphic, [new_rule, rule], patience=120)
+    if new_rule.lhs in grammar.rule_dict:
+        for rule in grammar.rule_dict[new_rule.lhs]:
+            message, is_isomorphic = timeout(lambda x, y: x == y, [new_rule, rule], patience=120)
 
-                # if isomorphism check times out, then pretend like it failed and move on
-                if not message and result:
-                    rule.frequency += 1
-                    grammar.rule_tree[which_parent][0] = rule
-                    grammar.temporal_matrix[parent_idx, find(rule, grammar.rule_list)[0]] += 1
-                    return
-
-                # if timed out, mark this rule
-                if message:
-                    new_rule.timed_out = True
-                    print(message)
-
-    if mode == 'hash':  # need to evaluate whether or not to deprecate this mode
-        for parent_idx, other_rule in enumerate(grammar.rule_list):
-            if parent_rule.hash_equals(other_rule):
-                break
-
-        for new_idx, other_rule in enumerate(grammar.rule_list):
-            if new_rule.hash_equals(other_rule):
-                grammar.rule_list[new_idx].frequency += 1
-                grammar.rule_tree[which_parent][0] = grammar.rule_list[new_idx]
-                grammar.temporal_matrix[parent_idx, new_idx] += 1  # pylint: disable=undefined-loop-variable
+            # if new_rule is isomorphic to a rule that already exists, merge them
+            if not message and is_isomorphic:
+                rule.frequency += 1
+                grammar.rule_tree[which_parent][0] = rule
+                grammar.temporal_matrix[parent_idx, find(rule, grammar.rule_list)[0]] += 1
                 return
 
+            # if the isomorphism check times out, mark this rule and find the edit distance
+            if message:
+                edit_dist = grammar.minimum_edit_dist(new_rule)
+                new_rule.edit_dist = edit_dist - penalty
+                new_rule.timed_out = True
+                print(message)
+
     # this rule does not already exist in the grammar
-    new_rule.edit_dist += 1
-    if decouple:  # if new_rule modifies a rule learned at previous timestep
-        new_idx = len(grammar.rule_list)  # the index corresponding to the new rule
-        grammar.temporal_matrix = np.append(grammar.temporal_matrix,
-                                            np.zeros((1, new_idx)),
-                                            axis=0)
-        grammar.temporal_matrix = np.append(grammar.temporal_matrix,
-                                            np.zeros((new_idx + 1, 1)),
-                                            axis=1)
-
-        grammar.rule_list.append(new_rule)
-        grammar.rule_tree[which_parent][0] = new_rule
-
-        if new_rule.lhs in grammar.rule_dict:
-            grammar.rule_dict[new_rule.lhs].append(new_rule)
-        else:
-            grammar.rule_dict[new_rule.lhs] = [new_rule]
-
-        grammar.temporal_matrix[parent_idx, new_idx] += 1
-    else:
-        grammar.replace_rule(parent_rule, new_rule)
-
+    new_rule.edit_dist += penalty
+    grammar.replace_rule(parent_rule, new_rule)
     return
+
+
+def deprecated():
+    raise NotImplementedError
+
+    # if mode == 'hash':  # need to evaluate whether or not to deprecate this mode
+    #     for parent_idx, other_rule in enumerate(grammar.rule_list):
+    #         if parent_rule.hash_equals(other_rule):
+    #             break
+
+    #     for new_idx, other_rule in enumerate(grammar.rule_list):
+    #         if new_rule.hash_equals(other_rule):
+    #             grammar.rule_list[new_idx].frequency += 1
+    #             grammar.rule_tree[which_parent][0] = grammar.rule_list[new_idx]
+    #             grammar.temporal_matrix[parent_idx, new_idx] += 1  # pylint: disable=undefined-loop-variable
+    #             return
+
+    # if decouple:  # if new_rule modifies a rule learned at previous timestep
+    #     new_idx = len(grammar.rule_list)  # the index corresponding to the new rule
+    #     grammar.temporal_matrix = np.append(grammar.temporal_matrix,
+    #                                         np.zeros((1, new_idx)),
+    #                                         axis=0)
+    #     grammar.temporal_matrix = np.append(grammar.temporal_matrix,
+    #                                         np.zeros((new_idx + 1, 1)),
+    #                                         axis=1)
+
+    #     grammar.rule_list.append(new_rule)
+    #     grammar.rule_tree[which_parent][0] = new_rule
+
+    #     if new_rule.lhs in grammar.rule_dict:
+    #         grammar.rule_dict[new_rule.lhs].append(new_rule)
+    #     else:
+    #         grammar.rule_dict[new_rule.lhs] = [new_rule]
+
+    #     grammar.temporal_matrix[parent_idx, new_idx] += 1
+    # else:
+    #     grammar.replace_rule(parent_rule, new_rule)