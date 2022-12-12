6f87208e37fc42c23b298570b0f89439024e5bf9
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2022-12-12 17:23:50 -0500

    [update] Let's try this out.
     Changes to be committed:
    	modified:   experiment_saturation.py

--- experiment_saturation.py
+++ experiment_saturation.py
@@ -4,13 +4,9 @@ from os import getcwd
 from os.path import join
 from argparse import ArgumentParser
 
-import sys
-sys.path.append('./src/')
-
 import git
 import numpy as np
 import networkx as nx
-from tqdm import tqdm
 from joblib import Parallel, delayed
 
 from cnrg.VRG import VRG
@@ -26,103 +22,98 @@ def experiment(trial: int, time_graph_pairs: list[tuple[int, nx.Graph]], mu: int
 
     base_grammar = decompose(base_graph, time=base_time, mu=mu)
 
-    prev_joint_grammar = update_grammar(base_grammar, base_graph, graph, time, mode='j')
-    joint_grammars = {(base_time, time): prev_joint_grammar}
+    prev_i_grammar = update_grammar(base_grammar, base_graph, graph, time, 'i')
+    i_grammars = {(base_time, time): prev_i_grammar}
 
-    prev_indep_grammar = update_grammar(base_grammar, base_graph, graph, time, mode='i')
-    indep_grammars = {(base_time, time): prev_indep_grammar}
+    prev_ja_grammar = update_grammar(base_grammar, base_graph, graph, time, 'ja')
+    ja_grammars = {(base_time, time): prev_ja_grammar}
+
+    prev_jb_grammar = update_grammar(base_grammar, base_graph, graph, time, 'jb')
+    jb_grammars = {(base_time, time): prev_jb_grammar}
 
     cumulative_graph = nx.compose(base_graph, graph)
     for next_time, next_graph in time_graph_pairs[2:]:
-        joint_grammar = update_grammar(prev_joint_grammar, cumulative_graph, next_graph, next_time, mode='j')
-        joint_grammars[(time, next_time)] = joint_grammar
+        i_grammar = update_grammar(prev_i_grammar, cumulative_graph, next_graph, next_time, 'i')
+        i_grammars[(time, next_time)] = i_grammar
 
-        indep_grammar = update_grammar(prev_indep_grammar, cumulative_graph, next_graph, next_time, mode='i')
-        indep_grammars[(time, next_time)] = indep_grammar
+        ja_grammar = update_grammar(prev_ja_grammar, cumulative_graph, next_graph, next_time, 'j')
+        ja_grammars[(time, next_time)] = ja_grammar
+
+        jb_grammar = update_grammar(prev_jb_grammar, cumulative_graph, next_graph, next_time, 'j')
+        jb_grammars[(time, next_time)] = jb_grammar
 
         time = next_time
-        prev_joint_grammar = joint_grammar
-        prev_indep_grammar = indep_grammar
+        prev_i_grammar = i_grammar
+        prev_ja_grammar = ja_grammar
+        prev_jb_grammar = jb_grammar
         cumulative_graph = nx.compose(cumulative_graph, next_graph)
 
-    return trial, base_grammar, joint_grammars, indep_grammars
+    return trial, base_grammar, i_grammars, ja_grammars, jb_grammars
 
 
-# TODO: implement saving intermediate results in case we need to stop the code
-def main(dataset, n_trials, parallel, n_jobs, mu):
-    rootpath = git.Repo(getcwd(), search_parent_directories=True).git.rev_parse("--show-toplevel")
-    resultspath = 'results/experiment_saturation/'
-    mkdir(join(rootpath, resultspath))
+def main(pre_dispatch: bool):
+    def tasks():
+        for trial in range(1, args.n_trials + 1):
+            yield (trial, time_graph_pairs, args.mu)
 
-    base_grammars: dict[int, VRG] = {}
-    joint_grammars: dict[int, dict[tuple[int, int], VRG]] = {}
-    indep_grammars: dict[int, dict[tuple[int, int], VRG]] = {}
+    def write(res):
+        for trial, base_grammar, i_grammars, ja_grammars, jb_grammars in res:
+            pickle.dump(base_grammar, base_gf)
+            pickle.dump(i_grammars, i_gf)
+            pickle.dump(ja_grammars, ja_gf)
+            pickle.dump(jb_grammars, jb_gf)
 
-    time_graph_pairs: list[tuple[int, nx.Graph]] = load_data(dataset)
+            base_mdlf.write(f'{trial},{base_grammar.mdl}\n')
+            for (curr_time, next_time), i_grammar in i_grammars.items():
+                i_mdlf.write(f'{trial},{curr_time},{next_time},{i_grammar.mdl}\n')
+                i_llf.write(f'{trial},{curr_time},{next_time},{i_grammar.ll}\n')
+            for (curr_time, next_time), ja_grammar in ja_grammars.items():
+                ja_mdlf.write(f'{trial},{curr_time},{next_time},{ja_grammar.mdl}\n')
+                ja_llf.write(f'{trial},{curr_time},{next_time},{ja_grammar.ll}\n')
+            for (curr_time, next_time), jb_grammar in jb_grammars.items():
+                jb_mdlf.write(f'{trial},{curr_time},{next_time},{jb_grammar.mdl}\n')
+                jb_llf.write(f'{trial},{curr_time},{next_time},{jb_grammar.ll}\n')
 
-    if parallel:
-        results = Parallel(n_jobs=n_jobs)(
-            delayed(experiment)(trial, time_graph_pairs, mu)
-            for trial in range(1, n_trials + 1)
-        )
+    time_graph_pairs: list[tuple[int, nx.Graph]] = load_data(args.dataset)
+
+    if args.batch:
+        batch = []
+        batch_size = 4 * args.n_jobs
+        for num, task in enumerate(tasks()):
+            batch.append(task)
+
+            if num % batch_size == 0:
+                if args.parallel:
+                    results = Parallel(n_jobs=args.n_jobs, verbose=10, pre_dispatch='all' if pre_dispatch else '2 * n_jobs')(
+                        delayed(experiment)(*task) for task in batch
+                    )
+                else:
+                    results = [experiment(*task) for task in batch]
+
+                batch = []
+                write(results)
+
+        # clean up last batch
+        if args.parallel:
+            results = Parallel(n_jobs=args.n_jobs, verbose=10, pre_dispatch='all' if pre_dispatch else '2 * n_jobs')(
+                delayed(experiment)(*task) for task in batch
+            )
+        else:
+            results = [experiment(*task) for task in batch]
+        write(results)
     else:
-        results = [experiment(trial, time_graph_pairs, mu)
-                   for trial in range(1, n_trials + 1)]
-
-    for trial, base_grammar, joint_dict, indep_dict in results:
-        base_grammars[trial] = base_grammar
-        joint_grammars[trial] = joint_dict
-        indep_grammars[trial] = indep_dict
-
-    base_mdls = {key: grammar.mdl
-                 for key, grammar in base_grammars.items()}
-
-    joint_mdls = {trial: {(t1, t2): grammar.mdl for (t1, t2), grammar in joint_dict.items()}
-                  for trial, joint_dict in joint_grammars.items()}
-    indep_mdls = {trial: {(t1, t2): grammar.mdl for (t1, t2), grammar in indep_dict.items()}
-                  for trial, indep_dict in indep_grammars.items()}
-
-    joint_lls = {trial: {(t1, t2): grammar.ll for (t1, t2), grammar in joint_dict.items()}
-                 for trial, joint_dict in joint_grammars.items()}
-    indep_lls = {trial: {(t1, t2): grammar.ll for (t1, t2), grammar in indep_dict.items()}
-                 for trial, indep_dict in indep_grammars.items()}
-
-    with open(join(rootpath, resultspath, f'{dataset}_base.grammars'), 'wb') as outfile:
-        pickle.dump(base_grammars, outfile)
-    with open(join(rootpath, resultspath, f'{dataset}_joint.grammars'), 'wb') as outfile:
-        pickle.dump(joint_grammars, outfile)
-    with open(join(rootpath, resultspath, f'{dataset}_indep.grammars'), 'wb') as outfile:
-        pickle.dump(indep_grammars, outfile)
-
-    with open(join(rootpath, resultspath, f'{dataset}_base.mdls'), 'w') as outfile:
-        outfile.write('trial,mdl\n')
-        for trial, mdl in base_mdls.items():
-            outfile.write(f'{trial},{mdl}\n')
-
-    with open(join(rootpath, resultspath, f'{dataset}_joint.mdls'), 'w') as outfile:
-        outfile.write('trial,t1,t2,mdl\n')
-        for trial, mdls in joint_mdls.items():
-            for (t1, t2), mdl in mdls.items():
-                outfile.write(f'{trial},{t1},{t2},{mdl}\n')
-    with open(join(rootpath, resultspath, f'{dataset}_indep.mdls'), 'w') as outfile:
-        outfile.write('trial,t1,t2,mdl\n')
-        for trial, mdls in indep_mdls.items():
-            for (t1, t2), mdl in mdls.items():
-                outfile.write(f'{trial},{t1},{t2},{mdl}\n')
-
-    with open(join(rootpath, resultspath, f'{dataset}_joint.lls'), 'w') as outfile:
-        outfile.write('trial,t1,t2,ll\n')
-        for trial, lls in joint_lls.items():
-            for (t1, t2), ll in lls.items():
-                outfile.write(f'{trial},{t1},{t2},{ll}\n')
-    with open(join(rootpath, resultspath, f'{dataset}_indep.lls'), 'w') as outfile:
-        outfile.write('trial,t1,t2,ll\n')
-        for trial, lls in indep_lls.items():
-            for (t1, t2), ll in lls.items():
-                outfile.write(f'{trial},{t1},{t2},{ll}\n')
+        if args.parallel:
+            results = Parallel(n_jobs=args.n_jobs, verbose=10, pre_dispatch='all' if pre_dispatch else '2 * n_jobs')(
+                delayed(experiment)(trial, time_graph_pairs, args.mu)
+                for trial in range(1, args.n_trials + 1)
+            )
+        else:
+            results = [experiment(trial, time_graph_pairs, args.mu)
+                       for trial in range(1, args.n_trials + 1)]
+        write(results)
 
 
-# python experiment_sequential_random.py [dataset] -d [delta] -r [rewire] -n [# trials] -p -j [# jobs] -m [mu]
+# nice -n 10 python experiment_random.py email-enron -r 0.25 -d 25 -n 25 -p -j 40 -m 4
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('dataset',
@@ -131,17 +122,22 @@ if __name__ == '__main__':
                         choices=['facebook-links', 'email-dnc', 'email-eucore', 'email-enron'],
                         help='select a dataset from ["facebook-links", "email-dnc", "email-eucore", "email-enron"]')
     parser.add_argument('-n', '--num',
-                        default=5,
+                        default=10,
                         dest='n_trials',
                         type=int,
                         help='the number of times to run each experiment')
+    parser.add_argument('-b', '--batch',
+                        action='store_true',
+                        default=False,
+                        dest='batch',
+                        help='batch the compute tasks or not')
     parser.add_argument('-p', '--parallel',
                         action='store_true',
                         default=False,
                         dest='parallel',
                         help='run the experiment in parallel or not')
     parser.add_argument('-j', '--jobs',
-                        default=40,
+                        default=10,
                         dest='n_jobs',
                         type=int,
                         help='the max number of parallel jobs to spawn')
@@ -151,4 +147,42 @@ if __name__ == '__main__':
                         type=int,
                         help='select a value for the μ hyperparameter for CNRG')
     args = parser.parse_args()
-    main(args.dataset, args.n_trials, args.parallel, args.n_jobs, args.mu)
+
+    rootpath = git.Repo(getcwd(), search_parent_directories=True).git.rev_parse("--show-toplevel")
+    resultspath = 'results/experiment_random/'
+    mkdir(join(rootpath, resultspath))
+
+    # pylint: disable=consider-using-with
+    base_gf = open(join(rootpath, resultspath, f'{args.dataset}_{args.mu}_base.grammars'), 'wb')
+    i_gf = open(join(rootpath, resultspath, f'{args.dataset}_{args.mu}_i.grammars'), 'wb')
+    ja_gf = open(join(rootpath, resultspath, f'{args.dataset}_{args.mu}_ja.grammars'), 'wb')
+    jb_gf = open(join(rootpath, resultspath, f'{args.dataset}_{args.mu}_jb.grammars'), 'wb')
+    base_mdlf = open(join(rootpath, resultspath, f'{args.dataset}_{args.mu}_base.mdls'), 'w')
+    i_mdlf = open(join(rootpath, resultspath, f'{args.dataset}_{args.mu}_i.mdls'), 'w')
+    ja_mdlf = open(join(rootpath, resultspath, f'{args.dataset}_{args.mu}_ja.mdls'), 'w')
+    jb_mdlf = open(join(rootpath, resultspath, f'{args.dataset}_{args.mu}_jb.mdls'), 'w')
+    i_llf = open(join(rootpath, resultspath, f'{args.dataset}_{args.mu}_i.lls'), 'w')
+    ja_llf = open(join(rootpath, resultspath, f'{args.dataset}_{args.mu}_ja.lls'), 'w')
+    jb_llf = open(join(rootpath, resultspath, f'{args.dataset}_{args.mu}_jb.lls'), 'w')
+
+    base_mdlf.write('trial,mdl\n')
+    i_mdlf.write('trial,time1,time2,mdl\n')
+    ja_mdlf.write('trial,time1,time2,mdl\n')
+    jb_mdlf.write('trial,time1,time2,mdl\n')
+    i_llf.write('trial,time1,time2,ll\n')
+    ja_llf.write('trial,time1,time2,ll\n')
+    jb_llf.write('trial,time1,time2,ll\n')
+
+    main(pre_dispatch=False)
+
+    base_gf.close()
+    i_gf.close()
+    ja_gf.close()
+    jb_gf.close()
+    base_mdlf.close()
+    i_mdlf.close()
+    ja_mdlf.close()
+    jb_mdlf.close()
+    i_llf.close()
+    ja_llf.close()
+    jb_llf.close()
