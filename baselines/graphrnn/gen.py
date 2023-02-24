3f65b99bfbf5a89a66c4fd78863faa61283a13c0
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2023-02-24 17:49:56 -0500

    modified:   baselines/graphrnn/gen.py 	modified:   graphrnn_runner.py

--- baselines/graphrnn/gen.py
+++ baselines/graphrnn/gen.py
@@ -6,6 +6,6 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '1,2'
 from baselines.graphrnn.train import *
 
 
-def gen(args, model, output, gen_num=1):
-    generated = test_rnn_epoch(0, args, model, output, test_batch_size=gen_num)
+def gen(args, model, output, num_gen=1):
+    generated = test_rnn_epoch(0, args, model, output, test_batch_size=num_gen)
     return generated
--- graphrnn_runner.py
+++ graphrnn_runner.py
@@ -1,4 +1,9 @@
+from os import getcwd, mkdir
+from os.path import join
+
+import git
 import torch
+import networkx as nx
 torch.cuda.set_device(0)
 
 from baselines.graphrnn.fit import fit
@@ -6,20 +11,45 @@ from baselines.graphrnn.gen import gen
 from src.data import load_data
 # from src.utils import mkdir
 
-loaded = load_data('facebook-links')
+
+dataset: str = input('dataset: ').lower()
+mode: str = input('static or dynamic? ').lower()
+num_gen: int = input('number of graphs to generate (at each timestep): ').lower()
+perturb: bool = False
+
+assert dataset in ('email-dnc', 'email-enron', 'email-eucore', 'facebook-links')
+assert mode in ('static', 'dynamic')
+assert isinstance(num_gen, int)
+
+rootpath = git.Repo(getcwd(), search_parent_directories=True).git.rev_parse("--show-toplevel")
+resultspath = f'results/graphs_{mode}/graphrnn/{dataset}'
+
+# makedirs(path, exist_ok=True)
+
+loaded = load_data(dataset)
 graphs = [g for _, g in loaded]
 
-for graph in graphs[2:]:
-    args, model, output = fit(10*[graph], nn='rnn')
-    generated_graphs = gen(args=args, model=model, output=output)
-    print('!!!!!!!!!!!!!!!!')
-    exit()
-    for g in generated_graphs:
-        print('\t', g.order(), g.size())
 
-# args, model, output = fit(graphs)
-# generated_graphs = gen(args=args, model=model, output=output)
-# 
-# print(len(graphs))
-# for graph in generated_graphs:
-#     print('!!!!!!!!!!!!!!!!', g.order(), g.size())
+def write_graph(g, filepath, filename):
+    with open(join(filepath, filename), 'w') as outfile:
+        nx.write_edgelist(g, outfile)
+
+
+if mode == 'static':  # static generation
+    for t, graph in enumerate(graphs):
+        input_graphs = 10 * [graph]
+        args, model, output = fit(input_graphs, nn='rnn')
+        generated_graphs = gen(args=args, model=model, output=output, num_gen=num_gen)
+        for trial, graph in enumerate(generated_graphs):
+            write_graph(graph, resultspath, f'{t}_{trial}.edgelist')
+else:  # dynamic generation
+    for t in range(len(graphs)):
+        counter = 1
+        input_graphs = graphs[:t + 1]
+        while len(input_graphs) < 10:
+            input_graphs.append(input_graphs[t - counter])
+            counter += 1
+        args, model, output = fit(input_graphs, nn='rnn')
+        generated_graphs = gen(args=args, model=model, output=output)
+        for trial, graph in enumerate(generated_graphs):
+            write_graph(graph, resultspath, f'{t}_{trial}.edgelist')
