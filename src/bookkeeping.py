308f6000a785661758ba7024752df6bca08b7469
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2023-02-28 00:40:45 -0500

    modified:   src/bookkeeping.py

--- src/bookkeeping.py
+++ src/bookkeeping.py
@@ -73,15 +73,17 @@ def unseal(grammar: VRG, idx: int, nts: int, time: int):
     if not idx:
         return
 
-    _, pidx, anode = grammar.decomposition[idx]
+    metarule, pidx, anode = grammar.decomposition[idx]
 
     # if metarule[time].graph.order() == 0:
     #     unseal(grammar, pidx, anode, time)
     unseal(grammar, pidx, anode, time)
 
     # assert nts not in metarule[time].graph
-    if nts not in grammar[idx][time].graph:
-        grammar[idx][time].graph.add_node(nts, b_deg=0, label=0)
+    if nts not in metarule[time].graph:
+        metarule[time].graph.add_node(nts, b_deg=0, label=0)
+    # if nts not in grammar[idx][time].graph:
+    #     grammar[idx][time].graph.add_node(nts, b_deg=0, label=0)
 
 
 def propagate_ancestors(nts: str, rule_idx: int, child_lhs: int, grammar: VRG,
