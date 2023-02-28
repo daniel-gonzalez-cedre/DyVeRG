72cc2c324cb12f505092cc4e6fb55715a631b46a
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2023-02-28 07:41:51 -0500

    modified:   src/adjoin_rule.py

--- src/adjoin_rule.py
+++ src/adjoin_rule.py
@@ -27,9 +27,11 @@ def domestic(grammar: VRG, u: int, v: int, t1: int, t2: int, mode: str):  # TODO
 
     uparent_idx, uparent_metarule, ancestor_u = ancestor(u, grammar)
     if ancestor_u not in uparent_metarule[t2].graph:
+        assert mode != 'del'
         # if uparent_metarule[t2].graph.order() == 0:
         unseal(grammar,
-               grammar.decomposition[uparent_idx][1],
+               # grammar.decomposition[uparent_idx][1],
+               uparent_idx,
                grammar.decomposition[uparent_idx][2],
                t2)
         uparent_metarule[t2].graph.add_node(ancestor_u, b_deg=0)
@@ -37,9 +39,11 @@ def domestic(grammar: VRG, u: int, v: int, t1: int, t2: int, mode: str):  # TODO
 
     vparent_idx, vparent_metarule, ancestor_v = ancestor(v, grammar)
     if ancestor_v not in vparent_metarule[t2].graph:
+        assert mode != 'del'
         # if vparent_metarule[t2].graph.order() == 0:
         unseal(grammar,
-               grammar.decomposition[vparent_idx][1],
+               # grammar.decomposition[vparent_idx][1],
+               vparent_idx,
                grammar.decomposition[vparent_idx][2],
                t2)
         vparent_metarule[t2].graph.add_node(ancestor_v, b_deg=0)
