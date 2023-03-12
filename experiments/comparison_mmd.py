a074f4d9222081cdfbf00b5ea171e80295f586d8
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2023-03-12 18:26:21 -0400

    modified:   ../experiments/comparison_mmd.py 	modified:   likelihoods/coauth-dblp_cl_static_False.ll 	modified:   likelihoods/coauth-dblp_dyverg_incremental_False.ll 	modified:   likelihoods/coauth-dblp_er_static_False.ll 	modified:   likelihoods/coauth-dblp_verg_static_False.ll 	modified:   likelihoods/email-eucore_graphrnn_dynamic_False.ll 	modified:   likelihoods/email-eucore_graphrnn_static_False.ll 	modified:   likelihoods/facebook-links_cl_static_False.ll 	modified:   likelihoods/facebook-links_dyverg_incremental_False.ll 	modified:   likelihoods/facebook-links_er_static_False.ll 	modified:   likelihoods/facebook-links_sbm_static_False.ll 	modified:   likelihoods/facebook-links_verg_static_False.ll 	new file:   pds/coauth-dblp_cl_static.pd 	new file:   pds/coauth-dblp_dyverg_incremental.pd 	new file:   pds/coauth-dblp_er_static.pd 	new file:   pds/coauth-dblp_sbm_static.pd 	new file:   pds/coauth-dblp_verg_static.pd 	new file:   pds/email-dnc_cl_static.pd 	new file:   pds/email-dnc_dyverg_incremental.pd 	new file:   pds/email-dnc_er_static.pd 	new file:   pds/email-dnc_verg_static.pd 	new file:   pds/email-enron_cl_static.pd 	new file:   pds/email-enron_dyverg_incremental.pd 	new file:   pds/email-enron_er_static.pd 	new file:   pds/email-enron_sbm_static.pd 	new file:   pds/email-enron_verg_static.pd 	new file:   pds/email-eucore_cl_static.pd 	new file:   pds/email-eucore_dyverg_incremental.pd 	new file:   pds/email-eucore_er_static.pd 	new file:   pds/email-eucore_sbm_static.pd 	new file:   pds/email-eucore_verg_static.pd 	new file:   pds/facebook-links_cl_static.pd 	new file:   pds/facebook-links_dyverg_incremental.pd 	new file:   pds/facebook-links_er_static.pd 	new file:   pds/facebook-links_sbm_static.pd 	new file:   pds/facebook-links_verg_static.pd

--- experiments/comparison_mmd.py
+++ experiments/comparison_mmd.py
@@ -16,54 +16,75 @@ from src.utils import mkdir
 
 def compare_mmd(t: int, truegraph: nx.Graph, statistic, modeldataprefix: str, njobs: int = 1) -> list[float]:
     truestat = statistic(truegraph)
+    modelstats = []
 
-    for modeltrial in trange(10, desc=f'time {t}'):
+    for modeltrial in trange(10, desc=f'{model} {dataset} {statname} {t}'):
         try:
             modeldatafilename = f'{modeldataprefix}_{modeltrial}.edgelist'
             modelgraph = nx.read_edgelist(modeldatafilename)
         except FileNotFoundError:
             return []
 
-        modelstat = statistic(modelgraph)
+        modelstats.append(statistic(modelgraph))
 
-        yield maximum_mean_discrepancy(truestat, modelstat)
+        yield maximum_mean_discrepancy([truestat], modelstats)
 
 
 if __name__ == '__main__':
-    model: str = input('what model? ').strip().lower()
+    model, mode, statname, dataset = (None, None, None, None)
+
+    if len(sys.argv) > 1:
+        model = sys.argv[1]
+    if len(sys.argv) > 2:
+        mode = sys.argv[2]
+    if len(sys.argv) > 3:
+        statname = sys.argv[3]
+    if len(sys.argv) > 4:
+        dataset = sys.argv[4]
+
+    if not model:
+        model: str = input('what model? ').strip().lower()
+
     assert model in ('er', 'cl', 'sbm', 'graphrnn', 'verg', 'dyverg')
 
-    if model == 'dyverg':
-        mode: str = input('what learning mode? ').strip().lower()
-        assert mode in ('dynamic', 'incremental')
-    else:
-        mode: str = input('what generating mode? ').strip().lower()
-        assert mode in ('static', 'dynamic', 'incremental')
+    if not mode:
+        if model == 'dyverg':
+            mode: str = input('what learning mode? ').strip().lower()
+            assert mode in ('dynamic', 'incremental')
+        else:
+            mode: str = input('what generating mode? ').strip().lower()
+            assert mode in ('static', 'dynamic', 'incremental')
 
-    statname: str = input('what to measure? ').strip().lower().replace(' ', '_').replace('-', '_')
-    assert statname in ('degree_distribution', 'spectrum',  # vectors
-                        'average_degree', 'triangle_count', 'clustering', 'transitivity')  # scalars
+    assert mode in ('static', 'dynamic', 'incremental')
+
+    if not statname:
+        statname: str = input('what to measure? ').strip().lower().replace(' ', '-').replace('_', '-')
+
+    assert statname in ('degree-distribution', 'spectrum',  # vectors
+                        'average-degree', 'triangle-count', 'clustering', 'transitivity')  # scalars
     statfunc = (
-        degree_distribution if statname == 'degree_distribution' else
+        degree_distribution if statname == 'degree-distribution' else
         spectrum if statname == 'spectrum' else
-        average_degree if statname == 'average_degree' else
-        triangle_count if statname == 'triangle_count' else
+        average_degree if statname == 'average-degree' else
+        triangle_count if statname == 'triangle-count' else
         clustering if statname == 'clustering' else
         transitivity
     )
 
-    dataset: str = input('what dataset? ').strip().lower()
+    if not dataset:
+        dataset: str = input('what dataset? ').strip().lower()
+
     assert dataset in ('email-dnc', 'email-enron', 'email-eucore', 'facebook-links', 'coauth-dblp')
 
-    numjobs: int = int(input('number of parallel jobs? ').strip().lower())
+    # numjobs: int = int(input('number of parallel jobs? ').strip().lower())
 
     mu = 4
     clustering = 'leiden'
 
     rootpath = git.Repo(getcwd(), search_parent_directories=True).git.rev_parse("--show-toplevel")
     graphdir = join(rootpath, f'results/graphs_{mode}/{model}/{dataset}/')
-    resultdir = join(rootpath, 'results/mmds/')
-    resultfilename = join(resultdir, f'{dataset}_{model}_{mode}.mmd')
+    resultdir = join(rootpath, f'results/mmds/{statname}/')
+    resultfilename = join(resultdir, f'{dataset}_{model}_{mode}_{statname}.mmd')
     mkdir(resultdir)
 
     loaded = load_data(dataname=dataset)
@@ -73,7 +94,7 @@ if __name__ == '__main__':
     with open(resultfilename, 'w') as outfile:
         outfile.write('time,trial,mmd\n')
         for time in range(0, len(times)):
-            results = compare_mmd(time, graphs[time], statfunc, join(graphdir, f'{time}'), numjobs)
+            results = compare_mmd(time, graphs[time], statfunc, join(graphdir, f'{time}'))
 
             for trial, discrepancy in enumerate(results):
                 outfile.write(f'{time},{trial},{discrepancy}\n')
