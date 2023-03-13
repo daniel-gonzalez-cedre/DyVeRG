0aa7cbd9828a3e34cb33284bb9884c580d1724a9
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2023-03-13 18:52:57 -0400

    modified:   mmd_runner.py 	modified:   results/mmds/spectrum/coauth-dblp_cl_static_spectrum.mmd 	modified:   results/mmds/spectrum/coauth-dblp_er_static_spectrum.mmd 	modified:   results/mmds/spectrum/coauth-dblp_sbm_static_spectrum.mmd 	modified:   results/mmds/spectrum/coauth-dblp_verg_static_spectrum.mmd 	modified:   results/mmds/triangle-count/facebook-links_er_static_triangle-count.mmd

--- mmd_runner.py
+++ mmd_runner.py
@@ -1,21 +1,20 @@
 import subprocess
 
 
-# models = ('er', 'cl', 'sbm', 'verg', 'dyverg')
-models = ('dyverg',)
+models = ('er', 'cl', 'sbm', 'verg', 'dyverg')
+# models = ('dyverg',)
 datasets = ('email-dnc', 'email-enron', 'email-eucore', 'facebook-links', 'coauth-dblp')
-modes = {
-    'er': 'static',
-    'cl': 'static',
-    'sbm': 'static',
-    'graphrnn': 'static',  # ???
-    'verg': 'static',
-    'dyverg': 'incremental'  # ???
-}
-statnames = ('spectrum',)
-# statnames = ('degree-distribution',
-#              'average-degree', 'triangle-count', 'clustering', 'transitivity')
+# statnames = ('spectrum',)
+statnames = ('degree-distribution', 'average-degree', 'triangle-count', 'clustering', 'transitivity')
 for model in models:
     for dataset in datasets:
+        modes = {
+            'er': 'static',
+            'cl': 'static',
+            'sbm': 'static',
+            'graphrnn': 'incremental' if dataset == 'coauth-dblp' else 'static',
+            'verg': 'static',
+            'dyverg': 'incremental'
+        }
         for statname in statnames:
             subprocess.call(['python', 'experiments/comparison_mmd.py', model, modes[model], statname, dataset])
