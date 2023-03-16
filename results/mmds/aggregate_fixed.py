04809ab0d94ff0a6e936fe049734bc832406e8a6
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2023-03-16 03:10:22 -0400

    new file:   results/likelihoods/aggregateCOAUTHDBLP.ll_ranks 	new file:   results/likelihoods/aggregateEMAILDNC.ll_ranks 	new file:   results/likelihoods/aggregateEMAILENRON.ll_ranks 	new file:   results/likelihoods/aggregateEMAILEUCORE.ll_ranks 	new file:   results/likelihoods/aggregateFACEBOOKLINKS.ll_ranks 	new file:   results/mmds/aggregateCOAUTHDBLPaveragedegree.mmd 	new file:   results/mmds/aggregateCOAUTHDBLPclustering.mmd 	new file:   results/mmds/aggregateCOAUTHDBLPtransitivity.mmd 	new file:   results/mmds/aggregateCOAUTHDBLPtrianglecount.mmd 	new file:   results/mmds/aggregateEMAILDNCaveragedegree.mmd 	new file:   results/mmds/aggregateEMAILDNCclustering.mmd 	new file:   results/mmds/aggregateEMAILDNCtransitivity.mmd 	new file:   results/mmds/aggregateEMAILDNCtrianglecount.mmd 	new file:   results/mmds/aggregateEMAILENRONaveragedegree.mmd 	new file:   results/mmds/aggregateEMAILENRONclustering.mmd 	new file:   results/mmds/aggregateEMAILENRONtransitivity.mmd 	new file:   results/mmds/aggregateEMAILENRONtrianglecount.mmd 	new file:   results/mmds/aggregateEMAILEUCOREaveragedegree.mmd 	new file:   results/mmds/aggregateEMAILEUCOREclustering.mmd 	new file:   results/mmds/aggregateEMAILEUCOREtransitivity.mmd 	new file:   results/mmds/aggregateEMAILEUCOREtrianglecount.mmd 	new file:   results/mmds/aggregateFACEBOOKLINKSaveragedegree.mmd 	new file:   results/mmds/aggregateFACEBOOKLINKSclustering.mmd 	new file:   results/mmds/aggregateFACEBOOKLINKStransitivity.mmd 	new file:   results/mmds/aggregateFACEBOOKLINKStrianglecount.mmd 	new file:   results/mmds/aggregate_fixed.py

---
+++ results/mmds/aggregate_fixed.py
@@ -0,0 +1,56 @@
+import sys
+from os import getcwd
+from os.path import join
+sys.path.extend(['./', '../', '../../'])
+
+from tqdm import tqdm
+import git
+import numpy as np
+import scipy.stats as st
+
+
+def confidence(data: list[float], level: float = 0.95):
+    return st.sem(data) * st.t.ppf((1 + level) / 2.0, len(data) - 1)
+
+
+def interval(data: list[float]):
+    return st.t.interval(0.95, len(data) - 1, loc=np.mean(data), scale=st.sem(data))
+
+
+if __name__ == '__main__':
+    rootpath = git.Repo(getcwd(), search_parent_directories=True).git.rev_parse("--show-toplevel")
+    models = ['er', 'cl', 'sbm', 'graphrnn', 'verg', 'dyverg']
+    datasets = ['email-dnc', 'email-enron', 'email-eucore', 'facebook-links', 'coauth-dblp']
+    statistic = input('statistic: ').strip().lower().replace('_', '-').replace(' ', '-')
+    assert statistic in ('degree-distribution', 'spectrum', 'average-degree', 'clustering', 'transitivity', 'triangle-count')
+
+    for dataset in tqdm(datasets):
+        mode = {
+            'er': 'static',
+            'cl': 'static',
+            'sbm': 'static',
+            'graphrnn': 'incremental' if dataset == 'coauth-dblp' else 'static',
+            'verg': 'static',
+            'dyverg': 'incremental'
+        }
+        with open(join(rootpath, f'results/mmds/aggregate{dataset.replace("-", "").upper()}{statistic.replace("-", "").lower()}.mmd'), 'w') as outfile:
+            for model in models:
+                outfile.write(r'\pgfplotstableread{')
+                outfile.write('\nts mmd\n')
+
+                datatensor = {}
+                with open(join(rootpath, f'results/mmds/{statistic}/{dataset}_{model}_{mode[model]}_{statistic}.mmd_fixed'), 'r') as infile:
+                    next(infile)
+                    for line in infile:
+                        time, mmd = line.strip().split(',')
+                        datatensor[int(time)] = float(mmd)
+
+                for t in datatensor:
+                    if t == 0 or t > 10:
+                        outfile.write(f'% {t} {datatensor[t]}\n')
+                    else:
+                        outfile.write(f'{t} {datatensor[t]}\n')
+
+                outfile.write(fr'}}{{\{statistic.replace("-", "")}{dataset.replace("-", "").upper()}{model}}}')
+                outfile.write('\n\n')
+
